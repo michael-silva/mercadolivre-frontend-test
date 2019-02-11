960c604da65e6013dbf0ceda372c5a5f36cca9d9
Michael Silva <michael.silva@iteris.com.br>
2019-02-11 16:13:32 -0200

    add enviroment variable to api url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  API_URL: 'http://localhost:5000',
+  API_URL: process.env.API_URL || 'http://localhost:5000',
   GLOBAL_TITLE: 'Mercado Livre',
   DEFAULT_DESC: 'A maior Comunidade de compra e venda online da América Latina.',
 }
